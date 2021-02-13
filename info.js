fefa73b9863a7f44eb5cc6fd1565b946b64eaff5
vlad <vladov3000@gmail.com>
2021-02-13 00:14:13 -0800

    first commit

---
+++ info.js
@@ -0,0 +1,4 @@
+import * as portAudio from 'naudiodon';
+
+console.log(portAudio.getDevices());
+console.log(portAudio.getHostAPIs());
---
+++ play.js
@@ -0,0 +1,21 @@
+import * as portAudio from 'naudiodon';
+import * as fs from 'fs';
+
+// Create an instance of AudioIO with outOptions (defaults are as below), which will return a WritableStream
+var ao = new portAudio.AudioIO({
+  outOptions: {
+    channelCount: 2,
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 44100,
+    deviceId: 4, // Use -1 or omit the deviceId to select the default device
+    closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
+  }
+});
+
+// Create a stream to pipe into the AudioOutput
+// Note that this does not strip the WAV header so a click will be heard at the beginning
+var rs = fs.createReadStream('rawAudio.raw');
+
+// Start piping data and start streaming
+rs.pipe(ao);
+ao.start();
